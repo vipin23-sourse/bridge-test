3c6f8611b3dea7af0d97d2f35fbcba35281a074b
vipin <vipin.m2@gmail.com>
2022-05-13 15:24:46 +0530

    updated

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+"use strict"
+
+$(document).ready(function(){
+
+// mobile menu
+
+  $('.icon-menu').click(function () {
+    $('.mob-menu').addClass('slide-up')
+  });
+  
+  $('.closeBtn').click(function () {
+  
+    $('.mob-menu').removeClass('slide-up')
+  });
+  
+  $('.mob-menu ul li a').click(function () {
+    $('.mob-menu').removeClass('slide-up')
+  });
+
+// navbar fixed
+
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 300) {
+	    $(".header__wrap").css("padding" , "1.5rem 0");
+      $(".header__head").addClass("fixed-nav");
+	  }
+
+	  else{
+		  $(".header__wrap").css("padding" , "3.2rem 0");  
+      $(".header__head").removeClass("fixed-nav");
+	  }
+  })
+})
